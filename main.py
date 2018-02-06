2adaced8e9650f3e98231c3468d14f1e12414d7b
vedmedk0 <mp.golubtcov@physics.msu.ru>
2018-02-07 00:57:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://herokuProject_url/bot")
+    bot.set_webhook(url="https://floating-brook-95237.herokuapp.com//bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
